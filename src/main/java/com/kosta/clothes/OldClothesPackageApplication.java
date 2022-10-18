28b1d64a766d6cfb05e9a15b57acbe95f761598a
lillly02v <lilly02v@naver.com>
2022-10-18 13:32:03 +0900

    feat: Kosta Old Clothes

---
+++ src/main/java/com/kosta/clothes/OldClothesPackageApplication.java
@@ -0,0 +1,13 @@
+package com.kosta.clothes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OldClothesPackageApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OldClothesPackageApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/kosta/clothes/OldClothesPackageApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kosta.clothes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OldClothesPackageApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

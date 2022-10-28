1e60bbf0957bfc0bc2bda852c917d7e2288c1653
pigpiggy <hkim26586@gmail.com>
2022-10-28 12:03:24 +0900

    fear: made new files

---
+++ src/main/java/com/kosta/clothes/bean/sharing.java
@@ -0,0 +1,55 @@
+package com.kosta.clothes.bean;
+
+public class sharing {
+	private Long sid;
+	private String sname;
+	private String stitle;
+	private String scontent;
+	private String sstatus;
+	private int grade;
+	private int ai;
+	public Long getSid() {
+		return sid;
+	}
+	public void setSid(Long sid) {
+		this.sid = sid;
+	}
+	public String getSname() {
+		return sname;
+	}
+	public void setSname(String sname) {
+		this.sname = sname;
+	}
+	public String getStitle() {
+		return stitle;
+	}
+	public void setStitle(String stitle) {
+		this.stitle = stitle;
+	}
+	public String getScontent() {
+		return scontent;
+	}
+	public void setScontent(String scontent) {
+		this.scontent = scontent;
+	}
+	public String getSstatus() {
+		return sstatus;
+	}
+	public void setSstatus(String sstatus) {
+		this.sstatus = sstatus;
+	}
+	public int getGrade() {
+		return grade;
+	}
+	public void setGrade(int grade) {
+		this.grade = grade;
+	}
+	public int getAi() {
+		return ai;
+	}
+	public void setAi(int ai) {
+		this.ai = ai;
+	}
+	
+	
+}

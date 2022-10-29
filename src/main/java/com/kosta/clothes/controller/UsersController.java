8d68d6bf8fd8101f5d4d1badbf1af8b9dd38e224
lillly02v <lilly02v@naver.com>
2022-10-30 06:57:12 +0900

    fix: userscontroller error

--- src/main/java/com/kosta/clothes/controller/UsersController.java
+++ src/main/java/com/kosta/clothes/controller/UsersController.java
@@ -2,9 +2,9 @@ package com.kosta.clothes.controller;
 
 import java.util.Random;
 
-import org.apache.maven.model.Model;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMethod;

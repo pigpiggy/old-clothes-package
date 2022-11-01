7da25a0dfdf045647db406f62c8deeff647e6e51
JeongHaks <challenge0110@naver.com>
2022-11-01 23:56:16 +0900

    feat: project update

--- src/main/java/com/kosta/clothes/service/CertificationService.java
+++ src/main/java/com/kosta/clothes/service/CertificationService.java
@@ -1,42 +1,4 @@
-<<<<<<< HEAD
-=======
 
-//package com.kosta.clothes.service;
-//
-//import java.util.HashMap;
-//
-//import org.json.simple.JSONObject;
-//import org.springframework.stereotype.Service;
-//
-//import net.nurigo.java_sdk.exceptions.CoolsmsException;
-//
-//@Service
-//public class CertificationService {
-//	// 인증번호(전화번호, 인증번호)
-//    public void certifiedPhoneNumber(String phoneNumber, String cerNum) {
-//        String api_key = "NCSSTR2OR1HK2NFR";
-//        String api_secret = "XZ48XV4BD6MGF7DAAWJAG7PNSBMWKRLY";
-//        Message coolsms = new Message(api_key, api_secret);
-//
-//        HashMap<String, String> params = new HashMap<>();
-//        params.put("to", phoneNumber);    // 수신전화번호
-//        params.put("from", "01086963080");    // 발신전화번호
-//        params.put("type", "SMS");
-//        params.put("text", "하우헌옷의 인증번호 " + "["+cerNum+"]" + "를 입력하세요.");
-//        params.put("app_version", "test app 1.2"); // application name and version
-//
-//        try {
-//            JSONObject obj = coolsms.send(params);  // 문자 보내기
-//            System.out.println(obj.toString());
-//        } catch (CoolsmsException e) {  // 문자전송 실패 시 메세지
-//            System.out.println(e.getMessage());
-//            System.out.println(e.getCode());
-//        }
-//    }
-//
-//}
-
->>>>>>> 0699131dc67c9a76de2373c33b1fabab6c039ecc
 package com.kosta.clothes.service;
 
 import java.util.HashMap;
@@ -56,15 +18,24 @@ public class CertificationService implements UsersService {
 
 	@Autowired
 	UsersDAO usersDao;
+	
+	
+	//회원가입
+	@Override
+	public void insertUsers(Users users) throws Exception {
+		// TODO Auto-generated method stub
+		usersDao.insertUsers(users);
+	}
+	
 	// 인증번호(전화번호, 인증번호)
     public void certifiedPhoneNumber(String phoneNumber, String cerNum) {
-        String api_key = "NCSSTR2OR1HK2NFR"; //coolsms에서 발급받은 api_key
-        String api_secret = "XZ48XV4BD6MGF7DAAWJAG7PNSBMWKRLY";//coolsms에서 발급받은 api_secret key
+        String api_key = "NCSROJFYZ5NK1BXR"; //coolsms에서 발급받은 api_key
+        String api_secret = "PD5TJUAGKGMKRR3ZBZHZCFOPQLW38N53";//coolsms에서 발급받은 api_secret key
         Message coolsms = new Message(api_key, api_secret);
 
         HashMap<String, String> params = new HashMap<>();
         params.put("to", phoneNumber);    // 수신전화번호 
-        params.put("from", "01086963080");    // 발신전화번호 테스트 시에는 본인 번호 다 넣기 수신/발신 
+        params.put("from", "01051013402");    // 발신전화번호 테스트 시에는 본인 번호 다 넣기 수신/발신 
         params.put("type", "SMS");
         params.put("text", "하우헌옷의 인증번호 " + "["+cerNum+"]" + "를 입력하세요."); //문자 받을 때 문구 넣 을 곳 
         params.put("app_version", "test app 1.2"); // application name and version
@@ -99,9 +70,7 @@ public class CertificationService implements UsersService {
     	return true;
 	}
 
-<<<<<<< HEAD
-}
-=======
-}
 
->>>>>>> 0699131dc67c9a76de2373c33b1fabab6c039ecc
+
+
+}

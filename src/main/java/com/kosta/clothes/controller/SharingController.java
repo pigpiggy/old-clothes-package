bac96a58ec542a6b108d0f79aee05a24c1e22f6d
pigpiggy <hkim26586@gmail.com>
2022-11-06 14:06:33 +0900

    feat: modified sharingRegist

--- src/main/java/com/kosta/clothes/controller/SharingController.java
+++ src/main/java/com/kosta/clothes/controller/SharingController.java
@@ -1,18 +1,21 @@
 package com.kosta.clothes.controller;
 
-import java.io.File;
 import java.io.FileInputStream;
 import java.io.OutputStream;
+import java.util.ArrayList;
 import java.util.List;
 
 import javax.servlet.ServletContext;
 import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.util.FileCopyUtils;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -54,24 +57,52 @@ public class SharingController {
 	public ModelAndView registSharing(@ModelAttribute Sharing sharing, Model model,
 			@RequestParam("simageFile") MultipartFile[] files) {
 		ModelAndView mav = new ModelAndView();
-		try {
-			Integer sharingid = sharingService.registSharing(sharing, files);
-			Sharing sharingview = sharingService.viewSharing(sharingid);
-			model.addAttribute("title", sharing.getStitle());
-			model.addAttribute("content", sharing.getScontent());
-			String[] fidArr = sharingview.getSfileids().split(",");
-			
-			mav.addObject("files", fidArr);
-			mav.setViewName("redirect:/sharingList");
+		try { 
+			sharingService.registSharing(sharing, files);
+			System.out.println("registcontroller:" + sharing);
 		}catch(Exception e) {
 			e.printStackTrace();
 		}
 		return mav;
 	}
 	
-	@GetMapping("/sharingView")
-	public String sharingView() {
-		return "/sharing/sharingView";
+	@GetMapping("/sharingView/{sno}")
+	public ModelAndView viewSharing(@PathVariable("sno") Integer sno) {
+		ModelAndView mav = new ModelAndView();
+		try {
+			Sharing sharing = sharingService.viewSharing(sno);
+			String[] fidArr = sharing.getSfileids().split(","); //1,2,3이라는 문자열로 돼있으면 콤마로 잘라서 스트링 배열로 만들어줌 
+			//fidArr[0]="1",fidArr[1]="2", fidArr[2]="3"
+			List<Integer> fileList = new ArrayList<>();
+			for(String fid : fidArr) {
+				if(fid.trim().length() > 0) { //trim은 앞 뒤 스페이스 제거하고 비어있지 않으면
+					fileList.add(Integer.parseInt(fid)); //int로 바꿔서 넣는다
+				}
+			}
+			mav.addObject("files", fileList); 
+			mav.addObject("sharing", sharing);
+		}catch(Exception e) {
+			e.printStackTrace();
+		}
+		return mav;
 	}
 	
+	@GetMapping("/img/{sfileids}")
+	public void viewImage(@PathVariable Integer sfileids, HttpServletResponse response) {
+		String path = servletContext.getRealPath("/upload/");
+		FileInputStream fis = null;
+		try {
+			fis = new FileInputStream(path+sfileids);
+			OutputStream out = response.getOutputStream();
+			FileCopyUtils.copy(fis, out);
+		}catch(Exception e) {
+			e.printStackTrace();
+		}finally {
+			if(fis!=null) {
+				try {
+					fis.close();
+				} catch(Exception e) {}
+			}
+		}
+	}	
 }
--- src/main/java/com/kosta/clothes/service/SharingService.java
+++ src/main/java/com/kosta/clothes/service/SharingService.java
@@ -6,7 +6,8 @@ import java.util.List;
 import com.kosta.clothes.bean.Sharing;
 
 public interface SharingService {
-	Integer registSharing(Sharing sharing, MultipartFile[] files) throws Exception;
+//	Integer registSharing(Sharing sharing, MultipartFile[] files) throws Exception;
+	void registSharing(Sharing sharing, MultipartFile[] files) throws Exception;
 	Sharing viewSharing(Integer sno) throws Exception;
 	List<Sharing> getSharingList() throws Exception;
 }

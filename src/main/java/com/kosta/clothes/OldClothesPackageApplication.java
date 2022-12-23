c57b55b356dc1dc2cb16f09ddf64152423571266
KimMunJin <reminise057@gmail.com>
2022-12-23 14:35:02 +0900

    fix:timezone

--- src/main/java/com/kosta/clothes/OldClothesPackageApplication.java
+++ src/main/java/com/kosta/clothes/OldClothesPackageApplication.java
@@ -1,10 +1,20 @@
 package com.kosta.clothes;
 
+import java.util.Date;
+import java.util.TimeZone;
+
+import javax.annotation.PostConstruct;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class OldClothesPackageApplication {
+	@PostConstruct
+	public void started() {
+		TimeZone.setDefault(TimeZone.getTimeZone("Asia/Seoul"));
+		System.out.println("현재시각:" + new Date());
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(OldClothesPackageApplication.class, args);
